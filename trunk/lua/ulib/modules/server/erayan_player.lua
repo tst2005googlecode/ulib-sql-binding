bea057fddb1b16fe18b3b21b82a888436847fe76
erwindhaan@gmail.com <erwindhaan@gmail.com@64d4baa8-00a0-8984-3d97-c8b66f9377a8>
2011-03-14 15:11:46 +0000

    edited

--- trunk/lua/ulib/modules/server/erayan_player.lua
+++ trunk/lua/ulib/modules/server/erayan_player.lua
@@ -1,12 +1,19 @@
-function ULib.slap( ent, damage, power, nosound )
-	print('Slap override')
-end
--- way of overriding?
-
--- ban hook function
+-- slap/kick hook function
 function slapHook(ply, commandName, translated_args)
+    -- slap action
 	if commandName == 'ulx slap' then
 	print( 'Slapped someone, printed using ULibPostTranslatedCommand hook' )
+	for key, value in pairs(translated_args) do
+		print("Table entry #"..key..": "..value)
+	end
+	end
+	-- kick action
+	if commandName == 'ulx kick' then
+	print( 'Kicked someone, printed using ULibPostTranslatedCommand hook' )
+	for key, value in pairs(translated_args) do
+		print("Table entry #"..key..": "..value)
+	end
+
 	end
 end
 hook.Add("ULibPostTranslatedCommand", "EraYaNSlapHook", slapHook) -- or is there a ulib/ulx alternative
@@ -14,6 +21,6 @@ hook.Add("ULibPostTranslatedCommand", "EraYaNSlapHook", slapHook) -- or is there
 
 -- player join (add to mysql database)
 function playerJoinDB(ply)
-	print( 'player Joined' )
+	print( '---------------------A Player Joined---------------------' )
 end
 hook.Add("UCLAuthed", "HookName", playerJoinDB) -- or is there a ulib/ulx alternative
