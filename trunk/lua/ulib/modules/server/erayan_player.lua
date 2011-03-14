956abc2173d1d1f7c99fc59e2f767919b3cb4d98
erwindhaan@gmail.com <erwindhaan@gmail.com@64d4baa8-00a0-8984-3d97-c8b66f9377a8>
2011-03-14 15:35:41 +0000

    fixed for sub tables

--- trunk/lua/ulib/modules/server/erayan_player.lua
+++ trunk/lua/ulib/modules/server/erayan_player.lua
@@ -3,15 +3,37 @@ function commandHook(ply, commandName, translated_args)
     -- slap action
 	if commandName == 'ulx slap' then
 	print( 'Slapped someone, printed using ULibPostTranslatedCommand hook' )
+	--[[
+	translated args
+		1=slapper
+		2=user args
+	]]--
 	for key, value in pairs(translated_args) do
-		print( 'Table Slap' ,key, value)
+		if type(value) == 'table' then
+			for key, value in pairs(translated_args) do
+				print( 'Sub-Table Slap' ,key, value)
+			end		
+		else
+			print( 'Table Slap' ,key, value)
+		end
 	end
 	end
 	-- kick action
 	if commandName == 'ulx kick' then
 	print( 'Kicked someone, printed using ULibPostTranslatedCommand hook' )
+	--[[
+	translated args
+		1=kicker
+		2=user being kicked	
+	]]--
 	for key, value in pairs(translated_args) do
-		print( 'Table Kick' ,key, value)
+		if type(value) == 'table' then
+			for key, value in pairs(translated_args) do
+				print( 'Sub-Table Kick' ,key, value)
+			end		
+		else
+			print( 'Table Kick' ,key, value)
+		end
 	end
 
 	end
