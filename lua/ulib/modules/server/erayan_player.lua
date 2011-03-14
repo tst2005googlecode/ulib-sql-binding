2e1b118d7ad04deeda36385bedc78b6f4f1ea811
erwindhaan@gmail.com <erwindhaan@gmail.com@64d4baa8-00a0-8984-3d97-c8b66f9377a8>
2011-03-14 15:22:47 +0000

    fix concatenate

--- lua/ulib/modules/server/erayan_player.lua
+++ lua/ulib/modules/server/erayan_player.lua
@@ -1,26 +1,26 @@
 -- slap/kick hook function
-function slapHook(ply, commandName, translated_args)
+function commandHook(ply, commandName, translated_args)
     -- slap action
 	if commandName == 'ulx slap' then
 	print( 'Slapped someone, printed using ULibPostTranslatedCommand hook' )
 	for key, value in pairs(translated_args) do
-		print("Table entry #"..key..": "..value)
+		print( 'Table Slap' ,key, value)
 	end
 	end
 	-- kick action
 	if commandName == 'ulx kick' then
 	print( 'Kicked someone, printed using ULibPostTranslatedCommand hook' )
 	for key, value in pairs(translated_args) do
-		print("Table entry #"..key..": "..value)
+		print( 'Table Kick' ,key, value)
 	end
 
 	end
 end
-hook.Add("ULibPostTranslatedCommand", "EraYaNSlapHook", slapHook) -- or is there a ulib/ulx alternative
+hook.Add("ULibPostTranslatedCommand", "EraYaNCommandHook", commandHook)
 
 
 -- player join (add to mysql database)
 function playerJoinDB(ply)
 	print( '---------------------A Player Joined---------------------' )
 end
-hook.Add("UCLAuthed", "HookName", playerJoinDB) -- or is there a ulib/ulx alternative
+hook.Add("UCLAuthed", "PlayerAuthedHook", playerJoinDB)
