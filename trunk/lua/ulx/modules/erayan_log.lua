5beb29d576a99a8d1cbbc816c702425fb968f6b0
erwindhaan@gmail.com <erwindhaan@gmail.com@64d4baa8-00a0-8984-3d97-c8b66f9377a8>
2011-03-14 21:16:42 +0000

    log system nearly done

--- trunk/lua/ulx/modules/erayan_log.lua
+++ trunk/lua/ulx/modules/erayan_log.lua
@@ -9,12 +9,5 @@ function ulx.logStringOld( str, log_to_main )
 	if not ulx.log_file then return end
 
 	local date = os.date( "*t" )
-	if curDay ~= date.day then
-		next_log()
-	end
-
-	if log_to_main then
-		ServerLog( "[ULX] " .. str .. "\n" )
-	end
 	ulx.logWriteln( string.format( "[%02i:%02i:%02i] ", date.hour, date.min, date.sec ) .. str )
 end
