ad84774e06cf01b4e87ff1261ae15037c6345921
erwindhaan@gmail.com <erwindhaan@gmail.com@64d4baa8-00a0-8984-3d97-c8b66f9377a8>
2011-03-15 16:39:20 +0000

    log system works now

--- lua/ulx/modules/erayan_log.lua
+++ lua/ulx/modules/erayan_log.lua
@@ -1,3 +1,7 @@
+local logFile = ulx.convar( "logFile", "1", "", ULib.ACCESS_SUPERADMIN )
+local logDir  = ulx.convar( "logDir", "ulx_logs", "", ULib.ACCESS_SUPERADMIN )
+local next_log
+local curDay
 function ulx.logString( str, log_to_main )
 	-- database part below here
 	doAddLogItem(str);
@@ -5,9 +9,32 @@ function ulx.logString( str, log_to_main )
 	
 end
 
+local function next_log()
+	if logFile:GetBool() then
+		local new_log = os.date( logDir:GetString() .. "/" .. "%m-%d-%y" .. ".txt" )
+		if new_log == ulx.log_file then -- Make sure the date has changed.
+			return
+		end
+		local old_log = ulx.log_file
+		ulx.logWriteln( "<Logging continued in \"" .. new_log .. "\">" )
+		ulx.log_file = new_log
+		file.Write( ulx.log_file, "" )
+		ulx.logWriteln( "<Logging continued from \"" .. old_log .. "\">" )
+	end
+	curDay = os.date( "%d" )
+end
+
 function ulx.logStringOld( str, log_to_main )
 	if not ulx.log_file then return end
 
 	local date = os.date( "*t" )
+	if curDay ~= date.day then
+		next_log()
+	end
+
+	if log_to_main then
+		ServerLog( "[ULX] " .. str .. "\n" )
+	end
 	ulx.logWriteln( string.format( "[%02i:%02i:%02i] ", date.hour, date.min, date.sec ) .. str )
 end
+
